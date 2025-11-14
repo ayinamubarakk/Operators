7fbfa903d8f659ac94a9bcb3afaae9ae8cb5765a
Ayina Mubarak <ayina.mubarak2301051@gmail.com>
2025-11-14 22:51:57 +0500

    first commit

---
+++ lecture 2.py
@@ -0,0 +1,22 @@
+# WAYS TO WRITE STRINGS
+# str1 = "My name is Ayina Mubarak.\tI am 18 years old"
+# str2 = 'My name is Ayina Mubarak'
+# str3 = """My name is Ayina Mubarak"""
+# print(str1)
+# print(str2)
+# print(str3)
+
+# Concatenation
+# str1 = "Ayina"
+# str2 = "Mubarak"
+# print(str1 + str2)
+
+str1 = "ayina"
+print(len(str1))
+str2 = "mubarak"
+print(len(str2))
+
+
+
+
+
